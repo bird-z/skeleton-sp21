f704df311cf8c8124242978efca4419ceea17888
zxpen <z.x.peng@outlook.com>
2025-10-24 16:26:44 +0800

    finish ArrayDeque

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,107 @@
+package deque;
+
+public class ArrayDeque<T> {
+    private T[]  arr= (T[]) new Object[8];
+    private int  size ,maxSize;
+    private int first ,last;
+
+    public ArrayDeque() {
+        size =0;
+        maxSize=8;
+        first=maxSize/2;
+        last=maxSize/2+1;
+    }
+
+    /**add T before of first*/
+    public void addFirst(T item){
+        if(size+2>maxSize){
+            addSize();
+        }
+        arr[(first+maxSize)%maxSize]=item;
+        first--;
+        size++;
+    }
+
+    /**add T back of list*/
+    public void addList(T item){
+        if(size+2>maxSize){
+            addSize();
+        }
+        arr[(last)%maxSize]=item;
+        last++;
+        size++;
+    }
+
+    /**reFirst*/
+    public T reFirst(){
+        if(size>20&&size<maxSize/4){
+            minesSize();
+        }
+        first++;
+        size--;
+        T temp = arr[(first+maxSize)%maxSize];
+        return temp;
+    }
+
+    /**reLast*/
+    public T reLast(){
+        if(size>20&&size<maxSize/4){
+            minesSize();
+        }
+        last--;
+        size--;
+        T temp=arr[last%maxSize];
+        return temp;
+    }
+
+    /**get first*/
+    public T getFirst(){
+        if(size>0)
+            return arr[(first+1)%maxSize];
+        else return null;
+    }
+
+    /**get last*/
+    public T getLast(){
+        if(size>0){
+            return arr[(last-1)%maxSize];
+        }
+        else return null;
+    }
+
+    /**resize */
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+
+        for(int i=maxSize/2,j=capacity/2;i>=first;i--,j--){
+            a[(j+capacity)%capacity]=arr[(i+maxSize)%maxSize];
+        }
+        for (int i = maxSize/2+1,j=capacity/2+1 ;i <=last; i++,j++) {
+            a[j%capacity]=arr[(i)%maxSize];
+        }
+        arr=a;
+        first=capacity/2-maxSize/2+first;
+        last=capacity/2+last-maxSize/2;
+        maxSize=capacity;
+    }
+
+    /**add max size of arr*/
+    private void addSize(){
+        resize(maxSize*10);
+    }
+
+    /**mines max size of arr*/
+    private void minesSize(){
+        resize(maxSize/2);
+    }
+
+    /**get size of arr*/
+    public int getSize(){
+        return size;
+    }
+
+    /**get maxsize of arr*/
+    public int getMaxSize(){
+        return maxSize;
+    }
+}
--- proj1/deque/text.java
+++ proj1/deque/text.java
@@ -4,36 +4,67 @@ import java.util.Iterator;
 
 public class text {
     public static void main(String[] args) {
-        LinkedListDeque<Integer> intList = new LinkedListDeque<>();
-        System.out.println(intList.getSize());
-        System.out.println(intList.isEmpty());
-        intList.addFirst(5);
-        intList.addFirst(10);
-        intList.addLast(15);
-        intList.addLast(9);
-        intList.addLast(6);
-        intList.addLast(3);
-        System.out.println(intList.getSize());
-        System.out.println(intList.isEmpty());
+//        LinkedListDeque<Integer> intList = new LinkedListDeque<>();
+//        System.out.println(intList.getSize());
+//        System.out.println(intList.isEmpty());
+//        intList.addFirst(5);
+//        intList.addFirst(10);
+//        intList.addLast(15);
+//        intList.addLast(9);
+//        intList.addLast(6);
+//        intList.addLast(3);
+//        System.out.println(intList.getSize());
+//        System.out.println(intList.isEmpty());
 //        for (int i = 0; i < 6; i++) {
 //            System.out.println(intList.removeFirst());
 //        }
 //        System.out.println(intList.getSize());
 //        System.out.println(intList.removeFirst());
-        Iterator<Integer> t=  intList.iterator();
-        LinkedListDeque<Integer> n=new LinkedListDeque<>();
-        Iterator<Integer> nn=n.iterator();
-        Iterator<Integer> tt= intList.iterator();
-        while(t.hasNext()){
-            n.addLast(t.next());
-        }
-        while(tt.hasNext()){
-            System.out.println(tt.next());
-        }
-        while (nn.hasNext()){
-            System.out.println(nn.next());
-        }
+//        Iterator<Integer> t=  intList.iterator();
+//        LinkedListDeque<Integer> n=new LinkedListDeque<>();
+//        Iterator<Integer> nn=n.iterator();
+//        Iterator<Integer> tt= intList.iterator();
+//        while(t.hasNext()){
+//            n.addLast(t.next());
+//        }
+//        while(tt.hasNext()){
+//            System.out.println(tt.next());
+//        }
+//        while (nn.hasNext()){
+//            System.out.println(nn.next());
+//        }
 
-        System.out.println(intList.equals(n));
+//        System.out.println(intList.equals(n));
+//        System.out.println((-2+5)%5);
+        ArrayDeque<Integer> arr=new ArrayDeque<>();
+        arr.addFirst(1);
+        arr.addFirst(2);
+        arr.addFirst(3);
+        arr.addFirst(4);
+        arr.addFirst(5);
+        arr.addFirst(6);
+        arr.addFirst(7);
+        arr.addFirst(8);
+        arr.addList(20);
+        arr.addList(30);
+        System.out.println(arr.getSize());
+        System.out.println(arr.getMaxSize());
+        for (int l = 0; l < 10; l++) {
+            System.out.println(arr.reFirst());
+            System.out.println(arr.getSize());
+            System.out.println(arr.getMaxSize());
+            System.out.println("##############");
+        }
+//        for (int i = 0; i < 6; i++) {
+//            System.out.println("%%");
+//            System.out.println(arr.reFirst());
+//        }
+//        System.out.println("####################");
+//        for (int i = 0; i < 6; i++) {
+//            System.out.println(arr.getLast());
+//        System.out.println(arr.getFirst());
+//        }
+//        System.out.println(arr.reFirst());
+//        System.out.println(arr.getLast());
     }
 }
